b34d16a74b08bb734c412ef7e9d843e5a82b6dc0
luanAfons0 <luan.hmatao@gmail.com>
2023-12-30 19:48:00 -0300

    Validation page start

--- nest-phones-frontend/src/app/page.tsx
+++ nest-phones-frontend/src/app/page.tsx
@@ -1,7 +1,27 @@
+'use client'
+
+import { Box, Button, Container, Paper } from "@mui/material"
+import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
+import { useRouter } from 'next/navigation'
+
 export default function Home() {
+  const router = useRouter()
+  
+  const submitForm = () =>{
+    console.log('Changing route')
+    router.push('/dashboard')
+  }
+
   return (
-    <>
-      <h1>Home page</h1>
-    </>
+    <Container maxWidth='xl' sx={{ height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <Paper elevation={5} sx={{ height: '70%', width: '40%', padding: 5 }}>
+        <Typography variant="h6" paddingBottom={2.5}>Informe a chave de entrada:</Typography>
+        <TextField id="outlined-basic" label="Outlined" variant="outlined" sx={{ width: "100%" }} />
+        <Box sx={{ marginTop: 2.5, width: '100%', display: 'flex', flexDirection: 'row-reverse' }}>
+          <Button onClick={submitForm} variant="contained">Submit</Button>
+        </Box>
+      </Paper>
+    </Container>
   )
 }
